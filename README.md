d6b930795ecdd477a17a46fce7d92a232950bc02
Ignasi Elgström <i.elgstrom@gmail.com>
2025-04-28 18:58:15 +0200

    updated changes

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ## Màster de Ciència de dades de la UOC - Visualització de dades
 
-Aquesta es la primera part de l'activitat de anàlisi de dades en el que s'agafen tres subconjunts de dades per poder fer una visualització sobre aquesta.
-Les fonts son les següents:
+Aquesta és la primera part de l'activitat d'anàlisi de dades en el que s'agafen tres subconjunts de dades per poder fer una visualització sobre aquesta.
+Les fonts són les següents:
 * https://www.kaggle.com/datasets/hashimkhanwazir/global-population-and-migration-dataset
 * https://www.kaggle.com/datasets/sazidthe1/global-happiness-scores-and-factors
 * https://ourworldindata.org/grapher/political-regime?time=latest
 * https://ourworldindata.org/women-rights
+
+Aquest repositori està compres per 
+1. carpeta dataset on es troben tots els csv amb les dades dels enllaços anteriors
+2. fitxer main.py per processar totes les dades
+3. fitxer .gitignore per no incloure contingut innecessari en el repositori
+4. fitxer final_data.csv resultant del processament de dades de main.py
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Llegim les dades
 df_migration = pd.read_csv('dataset/world_pop_mig_186_countries.csv')
 df_happiness_2015 = pd.read_csv('dataset/WHR_2015.csv')
 df_happiness_2016 = pd.read_csv('dataset/WHR_2016.csv')
@@ -12,28 +13,36 @@ df_happiness_2022 = pd.read_csv('dataset/WHR_2022.csv')
 df_happiness_2023 = pd.read_csv('dataset/WHR_2023.csv')
 df_political_regime = pd.read_csv('dataset/political-regime.csv')
 df_women = pd.read_csv("dataset/share-of-women-in-parliament.csv")
-# Suppose this is your mapping:
+
+# Fem el mapping segons la documentació:
 idx_to_regime = {0: 'closed autocracies', 1: 'electoral autocracies',
                  2: 'electoral democracies', 3: 'liberal democracies'}
 
-# Canviem el tipus de columna
+# Canviem el tipus de columna i els noms
 df_political_regime['Political regime'] = df_political_regime['Political regime'].map(idx_to_regime)
 df_political_regime['Political regime'] = df_political_regime['Political regime'].astype('category')
 df_political_regime = df_political_regime.rename(columns={'Entity': 'country', 'Year': 'year'})
-df_women = df_women.rename(columns={'Entity': 'country', 'Year': 'year'})
+df_women = df_women.rename(columns={'Entity': 'country', 'Year': 'year',
+                                    'Lower chamber female legislators (aggregate: average)': 'women_in_parlament'})
 
+# Afegim les dades de l'índex de felicitat a un df conjunt
 final_df = pd.DataFrame()
-for year in range(2015, 2024):
+for year in range(2015, 2024):  # agafem els limits de les dades del World Happiness Report
     df_name = f"df_happiness_{year}"
     df_year = globals()[df_name]
     df_subset = df_migration[df_migration['year'] == year]
     df = pd.merge(df_subset, df_year, on='country', how='left')
     final_df = pd.concat([final_df, df], ignore_index=True)
 
+# Adjuntem totes les dades
 final_df = (pd.merge(final_df, df_political_regime, on=['country', 'year'], how='left'))
-final_df = (pd.merge(final_df, df_women, on=['country', 'year'], how='left'))
+final_df = (pd.merge(final_df, df_women, on=['country', 'year', 'Code'], how='left'))
 
+# Eliminem els registres que no tenen prou dades
 not_data_countries = final_df[(final_df['happiness_score'].isna())]['country'].unique()
-
 final_df = final_df[final_df['happiness_score'].notna()]
+
+# Construïm el df resultant
 final_df.to_csv('final_data.csv')
+
+
